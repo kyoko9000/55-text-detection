e113ae33c1ac582fbd7d078c07b1e6afd848b12a
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-06-05 18:42:53 +0700

    update

--- easyocr_1.py
+++ easyocr_1.py
@@ -1,4 +1,4 @@
 import easyocr
 reader = easyocr.Reader(['en'], gpu=False)  # this needs to run only once to load the model into memory
-result = reader.readtext('bien_2.png', detail=0)
+result = reader.readtext('0Jl54.png', detail=0)
 print(result)
